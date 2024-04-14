43b0543d4de1a7f65a024c035bb87ddaac049e52
Jose A <jose-pr@coqui.dev>
2024-04-13 23:40:57 -0400

    HttpPath reorg

--- src/uripath/utils.py
+++ src/uripath/utils.py
@@ -2,28 +2,7 @@ import functools as _functools
 import typing as _ty
 import time as _time
 from email.utils import parsedate as _parsedate
-
-from htmllistparse import parse as _htmlparse
-import bs4 as _bs4
 import typing as _ty
-
-if _ty.TYPE_CHECKING:
-    import requests
-
-
-class FileEntry(_ty.NamedTuple):
-    name: str
-    modified: _ty.Optional[_time.struct_time]
-    size: _ty.Optional[int]
-    description: _ty.Optional[str]
-
-
-def ls(url, session: "requests.Session", **requests_args) -> list[FileEntry]:
-    req = session.get(url, **requests_args)
-    req.raise_for_status()
-    soup = _bs4.BeautifulSoup(req.content, "html5lib")
-    _, listing = _htmlparse(soup)
-    return listing
 
 
 def parsedate(date: _ty.Union[str, _time.struct_time, tuple, float]):
