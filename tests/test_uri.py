ab63b642ec62301a84cd63a07ec4363f2c03f011
Jose A. <jose-pr@coqui.dev>
2024-04-26 00:09:53 -0400

    better source handling

---
+++ tests/test_uri.py
@@ -0,0 +1,16 @@
+import pytest
+import src.pathlib_next as pathlib_next
+
+
+
+def test_scheme_and_host():
+    uri = pathlib_next.Uri('http://google.com')
+    assert uri.source.scheme == 'http'
+    assert uri.source.host == 'google.com'
+    assert uri.source.port == None
+
+def test_no_scheme_with_host():
+    uri = pathlib_next.Uri('//google.com/')
+    assert uri.source.scheme == None
+    assert uri.source.host == 'google.com'
+    assert uri.source.port == None
